9c79e0b94891f345b7e053907fde5c6702659584
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2019-08-28 18:17:58 +0200

    Use quiet build per default for emerge

--- scripts/portage.sh
+++ scripts/portage.sh
@@ -4,3 +4,7 @@ chroot /mnt/gentoo /bin/bash <<'EOF'
 mkdir /usr/portage
 emerge-webrsync
 EOF
+
+chroot /mnt/gentoo /bin/bash <<'EOF'
+echo 'EMERGE_DEFAULT_OPTS="--quiet-build=y"' >> /etc/portage/make.conf
+EOF
