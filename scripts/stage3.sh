5ccd424ff5429171fd558ce1bde5680f36f550da
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2019-08-28 18:17:28 +0200

    Updated archive format to XZ

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-tarball=stage3-amd64-nomultilib-$STAGE3.tar.bz2
+tarball=stage3-amd64-nomultilib-$STAGE3.tar.xz
 
 mount /dev/sda4 /mnt/gentoo
 
 cd /mnt/gentoo
 wget http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3/$tarball
-tar xvjpf $tarball
+tar xvJpf $tarball
 rm -f $tarball
